e5e5c9230d28f1e175a66eb3f284371905f127fd
Luciano Sabença <Lucianosabenca@gmail.com>
2012-04-21 16:36:42 -0300

    Improved messages.

--- AutomaticTest.sh
+++ AutomaticTest.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+]#!/bin/bash
 
 
 # Script para efetuar teste automaticos, comparando as respostas do sussy com as do seu programa
 # Author: Luciano Padua Sabenca CC 012
 echo
-echo '===== Script para efetuar teste do susy ====='
-echo 'Digite a Materia e turma(exemplo MC102wy):'
+echo '===== Script para efetuar testes automaticamente, baixando-os do Sussy ====='
+echo 'Digite a Materia e a(s) turma(s)(exemplo: MC102wy):'
 read link;
 echo 'Digite o numero do lab:'
 read nlab;
-echo 'Ha zero antes do numero do arquivo de teste(por exemplo arq01) sim ou nao ?'
+echo 'Ha zero antes do numero do arquivo de teste(por exemplo: arq01) sim/nao ?'
 read hazero
-echo 'Digite a quantidade de testes abertos para baixar:'
+echo 'Digite a quantidade de testes abertos que há para baixar:'
 read qtd;
 echo 'Digite o nome do programa compilado:'
 read nomePrograma;
-echo 'Começando a baixar os teste e saidas esperadas!'
+echo 'Começando a baixar os teste e as saidas esperadas!'
 
 sleep 5
 
@@ -42,9 +42,9 @@ do
   sleep 1
   ./$nomePrograma < $nomePrograma'_teste_'$a > $nomePrograma'_saida_'$a
   diff -s $nomePrograma'_saida_'$a $nomePrograma'_saida_prevista_'$a
-
-echo
+echo 'Script executado!'
 done;
+
 
 
 
