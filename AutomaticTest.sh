a28804d5e6dc462e3e8a68e6c453ef24b53f6941
Luciano Sabença <Lucianosabenca@gmail.com>
2012-04-22 23:08:52 -0300

    A lot of improvement.
    Thanks FernandoGolcaves..
    :)

--- AutomaticTest.sh
+++ AutomaticTest.sh
@@ -1,60 +1,64 @@
 #!/bin/bash
 
-
 # Script para efetuar teste automaticos, comparando as respostas do Susy com as do seu programa
-# Esse script baixa as entradas e saidas do susy e executa-as no programa ja compilado, comparando a saida do seu programa com a saida esperada.
+# Esse script baixa as entradas e saidas do susy, compila e executa seu programa, e compara as saidas do seu programa com as saidas esperadas.
+
+# Deve ser executado na pasta que contem o codigo fonte do programa
 
 
+# entradas: numero do lab (exemplo: 01, 03, 04b, 15)
+#           quantidade de testes abertos (exemplo 02 06 16)
+#           nome do programa (exemplo: 'programa', para um programa com nome programa.c)
 
-# Author: Luciano Padua Sabenca CC 012
+# saida: resultado do comando diff para as saidas do seu programa e as saidas esperadas
+
+# Author: Luciano P. Sabenca
+# Author: Fernando H. S. Goncalves
+
+echo '===== WELCOME TO [TESTER] :) ====='
 echo
-echo '===== Script para efetuar testes automaticamente, baixando-os do Susy ====='
-echo 'Digite a Materia e a(s) turma(s)(exemplo: mc102wy):'
-read link;
 echo 'Digite o numero do lab:'
 read nlab;
-echo 'Ha zero antes do numero do arquivo de teste(por exemplo: arq01) sim/nao ?'
-read hazero
 echo 'Digite a quantidade de testes abertos que há para baixar:'
-read qtd;
-echo 'Digite o nome do programa compilado:'
-read nomePrograma;
+read count;
+echo 'Digite o nome do programa:'
+read program;
+
 if [ ! -d "Lab_$nlab" ] ;then
    mkdir "Lab_$nlab"
 fi
 
-cp $nomePrograma "Lab_$nlab"
+gcc $program.c -o $program -Wall -ansi -pedantic -Werror # compila o programa e gera o executável
+
+mv  $program "Lab_$nlab"
 cd "Lab_$nlab"
 echo 'Começando a baixar os teste e as saidas esperadas!'
 sleep 3
 
-
-for ((a=00; a <= $qtd ; a++)); 
+for i in `seq -w 0 $count`;                #para i de 00 ateh count
 do 
-   if [ "$hazero" == "sim" ] ;  then
-      wget  -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq0'$a.'in' > $nomePrograma'_teste_'$a; 
-      wget  -O -   'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq0'$a.'res'  > $nomePrograma'_saida_prevista_'$a
-   else
-      wget -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq'$a.'in' > $nomePrograma'_teste_'$a; 
-      wget -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq'$a.'res'  > $nomePrograma'_saida_prevista_'$a;
-   fi
+      wget -O - 'http://susy.ic.unicamp.br:9999/mc102wy/'$nlab'/dados/arq'$i.'in' > $program'_teste_'$i ; 
+      wget -O - 'http://susy.ic.unicamp.br:9999/mc102wy/'$nlab'/dados/arq'$i.'res'  > $program'_saida_esperada_'$i ;
+
 done;
 
 
 echo 'Saidas e respostas baixadas!'
+echo
 
 echo 'Executando e comparando as saidas!'
 echo 
 sleep 3
 
-for ((a=0; a <= $qtd ; a++)); 
+for i in `seq -w 0 $count`;                #para i de 00 ateh count
 do
-  echo "==== Executando Teste $a ====" 
+  echo "==== Executando Teste $i ====" 
+  ./$program < $program'_teste_'$i > $program'_saida_'$i
   sleep 1
-  ./$nomePrograma < $nomePrograma'_teste_'$a > $nomePrograma'_saida_'$a
-  diff -s $nomePrograma'_saida_'$a $nomePrograma'_saida_prevista_'$a
+  diff -s $program'_saida_'$i $program'_saida_esperada_'$i 
+  echo
 done;
 
-echo
-echo 'Script executado!'
+echo '[TESTER] completed!'
+echo 'Copyleft Luciano P. Sabenca & Fernando H. S. Goncalves'
 
