8a34594a0eb61477d69290ad215d5e04ef68addf
Fernando Gonçalves <fernando.hsgoncalves@gmail.com>
2012-04-22 21:14:01 -0300

    updated to simplify fewer configurations

--- AutomaticTest.sh
+++ AutomaticTest.sh
@@ -1,60 +1,80 @@
 #!/bin/bash
 
-
 # Script para efetuar teste automaticos, comparando as respostas do Susy com as do seu programa
-# Esse script baixa as entradas e saidas do susy e executa-as no programa ja compilado, comparando a saida do seu programa com a saida esperada.
+# Esse script baixa as entradas e saidas do susy, compila e executa seu programa, e compara as saidas do seu programa com as saidas esperadas.
+
+# PROGRAMA DEVE SER EXECUTADO UMA PASTA ABAIXO DA PASTA DO LAB
+# exemplo:
+
+# ls susy
+# lab01
+# lab02
+# lab15
+# test.sh
+
+# entradas: numero do lab (exemplo: 01, 03, 04b, 15)
+#           quantidade de testes abertos (exemplo 02 06 16)
+#           nome do programa (exemplo: 'programa', para um programa com nome programa.c)
+
+# saida: resultado do comando diff para as saidas do seu programa e as saidas esperadas
 
 
-
-# Author: Luciano Padua Sabenca CC 012
+echo '[TESTER] start'
 echo
-echo '===== Script para efetuar testes automaticamente, baixando-os do Susy ====='
-echo 'Digite a Materia e a(s) turma(s)(exemplo: mc102wy):'
-read link;
-echo 'Digite o numero do lab:'
-read nlab;
-echo 'Ha zero antes do numero do arquivo de teste(por exemplo: arq01) sim/nao ?'
-read hazero
-echo 'Digite a quantidade de testes abertos que há para baixar:'
-read qtd;
-echo 'Digite o nome do programa compilado:'
-read nomePrograma;
-if [ ! -d "Lab_$nlab" ] ;then
-   mkdir "Lab_$nlab"
+echo 'numero do lab: '
+read lab;
+echo 'quantidade de testes: '
+read count;
+echo 'nome do programa: '
+read program;
+
+
+cd "lab$lab"
+if [ ! -d "dados" ] ;then
+    mkdir dados # se a pasta dados nao existe, vamos cria-la
 fi
 
-cp $nomePrograma "Lab_$nlab"
-cd "Lab_$nlab"
-echo 'Começando a baixar os teste e as saidas esperadas!'
-sleep 3
+cd dados  # entra na pasta dados
+ 
 
-
-for ((a=00; a <= $qtd ; a++)); 
-do 
-   if [ "$hazero" == "sim" ] ;  then
-      wget  -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq0'$a.'in' > $nomePrograma'_teste_'$a; 
-      wget  -O -   'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq0'$a.'res'  > $nomePrograma'_saida_prevista_'$a
-   else
-      wget -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq'$a.'in' > $nomePrograma'_teste_'$a; 
-      wget -O - 'http://susy.ic.unicamp.br:9999/'$link'/'$nlab'/dados/arq'$a.'res'  > $nomePrograma'_saida_prevista_'$a;
-   fi
-done;
-
-
-echo 'Saidas e respostas baixadas!'
-
-echo 'Executando e comparando as saidas!'
-echo 
-sleep 3
-
-for ((a=0; a <= $qtd ; a++)); 
-do
-  echo "==== Executando Teste $a ====" 
-  sleep 1
-  ./$nomePrograma < $nomePrograma'_teste_'$a > $nomePrograma'_saida_'$a
-  diff -s $nomePrograma'_saida_'$a $nomePrograma'_saida_prevista_'$a
-done;
+# agora vamos baixar todos os arquivos de testes abertos (entradas e saidas)
 
 echo
-echo 'Script executado!'
+echo '[i/o] downloading...'
+for i in `seq -w 0 $count`;                #para i de 00 ateh 19
+do
+    # so iremos baixar se ja nao tivermos baixado
+    if [ ! -f 'arq'$i'.in' ] ;then
+        wget http://susy.ic.unicamp.br:9999/mc102wy/$lab/dados/arq$i.in;   # baixa o arquivo de entrada
+    fi
+    if [ ! -f 'arq'$i'.res' ] ;then
+        wget http://susy.ic.unicamp.br:9999/mc102wy/$lab/dados/arq$i.res;  # baixa o arquivo de saida
+    fi
+done;
+echo
+echo '[i/o] download completed'
 
+cd .. 
+
+gcc $program.c -o $program -Wall -ansi -pedantic -Werror # compila o programa e gera o executável
+ 
+cd dados # entra no diretórios dos dados
+ 
+echo
+echo '[diff]'
+echo
+for i in `seq -w 0 $count`;               # variável i assume os valores 00,01 ,.., count
+     do        
+    ../redgreen < arq$i.in > arq$i.out;   # executa o programa que está um nível abaixo
+                                          # com a entrada arq%i.in e grava a saída no
+                                          # arquivo arq$i.out
+ 
+    echo '[ '$i' ]'
+    diff arq$i.out arq$i.res;  # compara o arquivo de saída com a resposta esperada
+    echo '[ /'$i' ]'
+    echo
+done                                 # fim do for
+
+echo '[TESTER] completed'
+echo 'copyleft Luciano P. Sabenca & Fernando H. S. Goncalves'
+# copyleft Luciano P. Sabenca & Fernando H. S. Goncalves
